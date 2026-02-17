bd14f24db931e330949b763baead1a65f3b789fc
AVANA Agence Immobiliere <avana.johnrayan@gmail.com>
2026-02-17 14:20:09 +0100

    Update announce.js

--- js/announce.js
+++ js/announce.js
@@ -1,6 +1,6 @@
 // Configuration Supabase
 const SUPABASE_URL = 'https://xdoqgounkfgpfjyvljxc.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhkb3Fnb3Vua2ZncGZqeXZsanhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzMzA0MzIsImV4cCI6MjA4NjkwNjQzMn0.-HEbJYKhUgpamlslKhmMkbLY1iERYtT_DTabjtIeAqU';
+const SUPABASE_ANON_KEY = 'sb_publishable_ERhyFUNm9ijnQlnL97WFZQ_yqrZJWmU';
 
 // Initialiser Supabase
 function initSupabase() {
